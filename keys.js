f209af6a075811c722318e696a8c8af4b282ecbc
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-02 15:45:43 -0400

    setup files

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+//Twitter
+var twitterKeys = {
+  consumer_key: '4LsmsvjIfKfZpx9E3egmCz8DY',
+  consumer_secret: 'HR8MWpxVRT2KmTkUbPVFol2r3Ds4R8SdKKyOajDA2cqrRgzG2i',
+  access_token_key: '926163597025308673-395iTyhASQB3F8lCicRq8Le7QqPwDNu',
+  access_token_secret: 'mPTi0BVS86gdFnXrdj7Ieea5LTvUGcBLZbP1xb1GsICjI',
+}
+
+module.exports = twitterKeys;
+
+//Spotify
+//...
---
+++ liri.js
@@ -0,0 +1,12 @@
+//Write the code you need to grab the data from keys.js
+//Then store the keys in a variable
+
+//Commands
+//Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    
+    //my-tweets : show last 20 tweets
+
+    //spotify-this-song '<song name here>': show song info (artist, song name, preview link, album) (default to "The Sign" by Ace of Base)
+        //sign up as dev for credentials
+    
+    //node liri.js movie-this '<movie name here>' : show movie data (default mr nobody)
