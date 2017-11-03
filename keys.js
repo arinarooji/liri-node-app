25798a9384017160b9634016d65b756670874dfc
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-03 14:29:09 -0400

    tweet retrieval

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
 //Twitter
-var twitterKeys = {
+var Twitter = require("twitter");
+var twitterKeys = new Twitter ({
   consumer_key: '4LsmsvjIfKfZpx9E3egmCz8DY',
   consumer_secret: 'HR8MWpxVRT2KmTkUbPVFol2r3Ds4R8SdKKyOajDA2cqrRgzG2i',
   access_token_key: '926163597025308673-395iTyhASQB3F8lCicRq8Le7QqPwDNu',
   access_token_secret: 'mPTi0BVS86gdFnXrdj7Ieea5LTvUGcBLZbP1xb1GsICjI',
-}
+});
 
 //Spotify
 var spotifyKeys = {
--- liri.js
+++ liri.js
@@ -6,6 +6,22 @@ var twitter = Keys.twitterKeys;
 var spotify = Keys.spotifyKeys;
 var omdb    = Keys.omdbKey;
 
+//Input (test)
+var task = process.argv[2];
+
+if (task.toLowerCase() === "my-tweets") {
+    twitter.get('statuses/user_timeline', function(error, tweets, response) {
+      if (!error) {
+        for (var i = 0; i < 20; i++) {
+            console.log(tweets[i].text, tweets[i].created_at);
+        }
+      }
+    });
+}
+else if (task.toLowerCase() === 'spotify') {
+    console.log('spotify this');
+}
+
 
 //Commands
 //Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
