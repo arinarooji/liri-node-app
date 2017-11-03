f8c115df3f25be002e5c1195f0ae1bc36ba4eee6
Keivon Arooji <karooji0@email.cpcc.edu>
2017-11-03 12:39:28 -0400

    all keys acquired

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('this is loaded');
-
 //Twitter
 var twitterKeys = {
   consumer_key: '4LsmsvjIfKfZpx9E3egmCz8DY',
@@ -8,7 +6,20 @@ var twitterKeys = {
   access_token_secret: 'mPTi0BVS86gdFnXrdj7Ieea5LTvUGcBLZbP1xb1GsICjI',
 }
 
-module.exports = twitterKeys;
-
 //Spotify
-//...
+var spotifyKeys = {
+  clientID: '010cd4ab4abb47ca88ba3e0b5cf696aa',
+  clientSecret: '94a9654ea42e4477aaeab69fe534ecb4'
+}
+
+//OMDB
+var omdbKey = {
+  apiKey: '40e9cece'
+}
+
+//Export keys
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys,
+  omdbKey: omdbKey
+}
--- liri.js
+++ liri.js
@@ -1,10 +1,17 @@
-//Write the code you need to grab the data from keys.js
+//Grab the data from keys.js
+var Keys = require('./keys.js');
+
 //Then store the keys in a variable
+var twitter = Keys.twitterKeys;
+var spotify = Keys.spotifyKeys;
+var omdb    = Keys.omdbKey;
+
 
 //Commands
 //Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
     
     //my-tweets : show last 20 tweets
+        //Read docs
 
     //spotify-this-song '<song name here>': show song info (artist, song name, preview link, album) (default to "The Sign" by Ace of Base)
         //sign up as dev for credentials
